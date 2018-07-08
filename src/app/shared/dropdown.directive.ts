73c5504eeeea7a8b53ae991eb78cca0b2b9c1ca0
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-07-09 02:07:58 +0800

    - building dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  private dropdownParentEl = this.elementRef.nativeElement.closest('.dropdown');
+  private isOpen:boolean;
+  constructor(private elementRef: ElementRef) { }
+
+//  @HostBinding('class.show') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      this.dropdownParentEl.classList.add('show');
+      this.dropdownParentEl.querySelector('.dropdown-menu').classList.add('show');
+    } else {
+      this.dropdownParentEl.classList.remove('show');
+      this.dropdownParentEl.querySelector('.dropdown-menu').classList.remove('show');
+    }
+  }
+}
