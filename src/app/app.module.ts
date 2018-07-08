93e18a884b231a3f8f1eaff0fd2223c7a7c61502
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-07-09 00:00:41 +0800

    - creating a basic attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import {BasicHighlightDirective} from './basic-highlight/basic-highlight-directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
