f36a366f5f3764d54f5906bf21d7ba43c856420c
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-07-29 20:44:01 +0800

    - signing users up

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+    signupUser(email: string, password: string) {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(
+          error => console.log(error)
+        );
+    }
+}
