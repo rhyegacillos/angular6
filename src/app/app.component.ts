77b1b23336802be04e0dbad1b75db02351e4a808
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-07-29 20:33:16 +0800

    - setting up the firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCCSHDyaohHz5xT5RsUeKKBxs3k6dyBF4g',
+      authDomain: 'angular-project-3ff3f.firebaseapp.com'
+    });
   }
 }
---
+++ src/app/auth/auth-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  constructor() { }
+
+  signupUser(email: string, password: string) {
+
+  }
+}
