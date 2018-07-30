2c4276e04fa832f87a4e32d15275a9b8978e45d6
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-07-30 23:44:37 +0800

    - added home page and lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {HomeComponent} from './home/home.component';
 const appRoutes: Routes = [
   // {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: '', component: HomeComponent},
- // {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
   {path: 'shopping-list', component: ShoppingListComponent}
 ]
 @NgModule({
