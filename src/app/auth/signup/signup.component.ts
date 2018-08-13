f6b33af9e0840b057a637570288c578b8e85d7d2
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-08-14 01:19:05 +0800

    - effects, signup authentication

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService} from '../auth.service';
+import * as fromApp from '../../store/app.reducer';
+import * as AuthActions from '../store/auth.actions';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'app-signup',
@@ -9,14 +11,12 @@ import {AuthService} from '../auth.service';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
   }
 
   onSignup(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;
-    this.authService.signupUser(email, password);
+    this.store.dispatch(new AuthActions.TrySignup(form.value));
   }
 }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,5 +1,9 @@
 import {Actions, Effect} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
+import * as AuthActions from './auth.actions';
+import {map, mergeMap, switchMap} from 'rxjs/operators';
+import * as firebase from 'firebase';
+import {from} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +11,30 @@ import {Injectable} from '@angular/core';
 export class AuthEffects {
 
   @Effect()
-  authSignup;
-
+  authSignup = this.actions
+    .ofType(AuthActions.TRY_SIGNUP)
+    .pipe(
+      map((action: AuthActions.TrySignup) => {
+        return action.payload;
+        }),
+      switchMap((authData: {username: string, password: string}) => {
+        return from(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+        }),
+      switchMap(() => {
+        return from(firebase.auth().currentUser.getIdToken());
+      }),
+      mergeMap((token: string) => {
+        return [
+          {
+            type: AuthActions.SIGNUP
+          },
+          {
+            type: AuthActions.SET_TOKEN,
+            payload: token
+          }
+        ];
+      })
+    )
   constructor(private actions: Actions) {}
 
 }
